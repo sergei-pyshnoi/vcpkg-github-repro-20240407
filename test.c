b9ddc2f67a0b04db9acd0e4d681e1972e2d0328e
Greg Domzalski <greg@yubico.com>
2024-04-16 10:19:38 -0700

    Update test.c

--- test.c
+++ test.c
@@ -1,8 +1,4 @@
-#include <libusb-1.0/libusb.h>
-
 void DoTheThing()
 {
-    libusb_context* ctx = libusb_init(NULL);
     // We're really not going to even get this far... so this isn't strictly necessary.
-    libusb_exit(ctx);
 }
