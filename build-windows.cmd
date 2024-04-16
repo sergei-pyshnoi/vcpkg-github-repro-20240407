b6722c1fc1a77b423c9b6c8f370466045ca1fdec
Greg Domzalski <greg@yubico.com>
2024-04-16 10:55:50 -0700

    Is it the static triplet?

--- build-windows.cmd
+++ build-windows.cmd
@@ -4,5 +4,5 @@ git restore .
 git pull
 popd
 
-cmake -S . -B build_out/cmake/win-x64 -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
+cmake -S . -B build_out/cmake/win-x64 -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build build_out/cmake/win-x64 --config Release
